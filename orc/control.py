ce2d8a1b25169f2eadb96b2169c531a0701182d7
exussum <exussum@gmail.com>
2025-09-28 10:15:16 -0400

    Internal orchestrator

---
+++ orc/control.py
@@ -0,0 +1,52 @@
+from enum import Enum
+import time
+import requests
+from datetime import datetime
+
+from orc import config
+
+
+def build_enum():
+    result = requests.get(f"{config.BASE_URL}/devices{config.ACCESS_TOKEN}")
+    return Enum(
+        "Light", {config.NAME_TO_HUBITAT[e["label"]]: e["id"] for e in result.json()}
+    )
+
+
+Lights = build_enum()
+
+
+def set_light(light, on=None, brightness=None):
+    def f():
+        if brightness:
+            requests.get(
+                f"{config.BASE_URL}/devices/{light.value}/setLevel/{brightness}{config.ACCESS_TOKEN}"
+            )
+        else:
+            print(
+                requests.get(
+                    f"{config.BASE_URL}/devices/{light.value}/{'on' if on else 'off'}{config.ACCESS_TOKEN}"
+                ).content
+            )
+
+    return f
+
+
+def build_schedule():
+    today = datetime.now()
+
+    sun_result = requests.get(f"{config.SUNRISE_URL}&date={today.date()}").json()[
+        "results"
+    ]
+    reset = today().replace(hour=1)
+    sunrise = datetime.fromisoformat(sun_result["sunrise"]).timedelta(hours=-1)
+    core_start = today.replace(hour=9, minute=15, second=0)
+    core_end = today.replace(hour=22, minute=30, second=0)
+
+    result = [(reset, set_light(e, on=False)) for e in Lights if e != Lights.BEDROOM_NIGHT_LIGHT]
+    result.append((sunrise, set_light(Lights.LIVING_ROOM_DESK_LAMP, on=True)))
+    result.append((sunrise, set_light(Lights.LIVING_ROOM_FLOOR_LAMP, on=True)))
+    result.append((core_start, set_light(Lights.BEDROOM_NIGHT_LIGHT, on=False)))
+    result.append((core_start, set_light(Lights.ENTANCE_DESK_LAMP, brightness=100)))
+    result.append((core_end, set_light(Lights.BEDROOM_NIGHT_LIGHT, on=True)))
+    return result
---
+++ orc/worker.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.date import DateTrigger
+from orc import control
+
+
+def add_day_jobs(scheduler):
+    for time, f in control.build_schedule():
+        scheduler.add_job(f, DateTrigger(time))
+
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler()
+
+    def f():
+        add_day_jobs(scheduler)
+
+    f()
+    scheduler.add_job(f, CronTrigger.from_crontab("30 0 * * *"))
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
