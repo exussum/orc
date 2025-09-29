c43fa76e543e095eb8245d9aebb8216424607c6a
exussum <exussum@gmail.com>
2025-09-28 22:52:20 -0400

    Add sound control

--- orc/config.py
+++ orc/config.py
@@ -4,7 +4,7 @@ BASE_URL = os.getenv("BASE_URL")
 ACCESS_TOKEN = "?access_token=" + os.getenv("ACCESS_TOKEN")
 SUNRISE_URL = os.getenv("SUNRISE_URL")
 
-NAME_TO_HUBITAT = {
+LIGHT_NAME_TO_HUBITAT = {
     "night light": "BEDROOM_NIGHT_LIGHT",
     "entrance desk lamp": "ENTANCE_DESK_LAMP",
     "kitchen lights": "KITCHEN_LIGHTS",
@@ -12,4 +12,10 @@ NAME_TO_HUBITAT = {
     "living room floor lamp": "LIVING_ROOM_FLOOR_LAMP",
     "office desk lamp": "OFFICE_DESK_LAMP",
     "office floor lamp": "OFFICE_FLOOR_LAMP",
+}
+
+AUDIO_NAME_TO_HUBITAT = {
+    "Living room mini": "LIVING_ROOM_MINI",
+    "Bedroom display": "BEDROOM_DISPLAY",
+    "Office display": "OFFICE_DISPLAY",
 }
