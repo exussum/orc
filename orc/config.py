3e4575091397ab8af27dc75ffa51821fc3cbcc71
exussum <exussum@gmail.com>
2025-11-28 13:54:30 -0500

    Add tests, implement mandatory routines

--- orc/config.py
+++ orc/config.py
@@ -1,16 +1,20 @@
 import os
 from datetime import datetime, timedelta
 from enum import Enum
+from zoneinfo import ZoneInfo
 
 import orc.model as m
 from orc import dal
 
-BASE_URL = os.getenv("BASE_URL")
-ACCESS_TOKEN = "?access_token=" + os.getenv("ACCESS_TOKEN")
-SUNRISE_URL = os.getenv("SUNRISE_URL")
-MARKET_HOLIDAYS_URL = os.getenv("MARKET_HOLIDAYS_URL")
+BASE_URL = os.getenv("BASE_URL", "http://base.example.com")
+ACCESS_TOKEN = "?access_token=" + os.getenv("ACCESS_TOKEN", "example-token")
+SUNRISE_URL = os.getenv("SUNRISE_URL", "http://sunrise.example.com")
+MARKET_HOLIDAYS_URL = os.getenv("MARKET_HOLIDAYS_URL", "http://holidays.exmaple.com")
+ENABLED = os.getenv("ENABLED", "")
 
-hubitat_config = dal.get_config()
+hubitat_config = dal.get_config() if ENABLED else {}
+
+TZ = ZoneInfo("America/New_York")
 
 
 def build_enum(name, hub_name_to_token, hubitat_config):
@@ -56,7 +60,7 @@ CONFIGS = m.scan(
     m.Theme(
         name="work day",
         configs=(
-            m.LightConfig(name="reset", when="1:00", what=set(Light) - {Light.BEDROOM_NIGHT_LIGHT}, state="off"),
+            m.LightConfig(name="reset", when="13:50", what=set(Light) - {Light.BEDROOM_NIGHT_LIGHT}, state="off", mandatory=True),
             m.LightConfig(name="partner up", when="6:15", what=[Light.LIVING_ROOM_FLOOR_LAMP, Light.KITCHEN_LIGHTS], state="on"),
             m.RoutineConfig(name="partner leaving", when="7:00", items=(
                 m.LightSubConfig(what=Light.ENTANCE_DESK_LAMP, state=1),
@@ -69,7 +73,7 @@ CONFIGS = m.scan(
                 m.LightSubConfig(what=[Light.BEDROOM_NIGHT_LIGHT, Light.KITCHEN_LIGHTS], state="off"),
             )),
             m.LightConfig(name="reset office light", when="9:05", what=Light.OFFICE_DESK_LAMP, state="off"),
-            m.LightConfig(name="sunset lights", when="sunset", what=[Light.BEDROOM_NIGHT_LIGHT, Light.KITCHEN_LIGHTS], state="on"),
+            m.LightConfig(name="sunset lights", when="sunset", what=[Light.BEDROOM_NIGHT_LIGHT, Light.KITCHEN_LIGHTS], state="on", mandatory=True),
             m.SoundConfig(name="quiet time", when="23:00", what=Sound, state=10),
         ),
     ),
@@ -84,7 +88,7 @@ CONFIGS = m.scan(
     m.Theme(
         name="day off",
         configs=(
-            m.LightConfig(name="reset", when="2:00", what=set(Light) - {Light.BEDROOM_NIGHT_LIGHT}, state="off"),
+            m.LightConfig(name="reset", when="13:50", what=set(Light) - {Light.BEDROOM_NIGHT_LIGHT}, state="off", mandatory=True),
             m.RoutineConfig(name="partner up", when="7:00", items=(
                 m.LightSubConfig(what=[Light.LIVING_ROOM_FLOOR_LAMP, Light.KITCHEN_LIGHTS], state="on"),
                 m.LightSubConfig(what=[Light.BEDROOM_NIGHT_LIGHT, Light.KITCHEN_LIGHTS], state="off"),
@@ -95,19 +99,19 @@ CONFIGS = m.scan(
                 m.LightSubConfig(what=[Light.LIVING_ROOM_DESK_LAMP, Light.LIVING_ROOM_FLOOR_LAMP], state="on"),
             )),
             m.LightConfig(name="reset office light", when="9:35", what=Light.OFFICE_DESK_LAMP, state="off"),
-            m.LightConfig(name="sunset lights", when="sunset", what=[Light.BEDROOM_NIGHT_LIGHT, Light.KITCHEN_LIGHTS], state="on"),
+            m.LightConfig(name="sunset lights", when="sunset", what=[Light.BEDROOM_NIGHT_LIGHT, Light.KITCHEN_LIGHTS], state="on", mandatory=True),
             m.SoundConfig(name="quiet time", when="23:00", what=Sound, state=10),
         ),
     ),
 )
 
-CONFIG_TV_LIGHTS = m.AdHocRoutineConfig(name="tv lights", items=(
+CONFIG_TV_LIGHTS = m.AdHocRoutineConfig(items=(
     m.LightSubConfig(what=[Light.LIVING_ROOM_DESK_LAMP, Light.LIVING_ROOM_FLOOR_LAMP, Light.OFFICE_FLOOR_LAMP], state="off"),
     m.LightSubConfig(what=[Light.ENTANCE_DESK_LAMP, Light.OFFICE_DESK_LAMP], state=1),
     m.LightSubConfig(what=Light.KITCHEN_LIGHTS, state="on")
 ))
 
-CONFIG_FRONT_ROOMS = m.AdHocRoutineConfig(name="front rooms", items=(
+CONFIG_FRONT_ROOMS = m.AdHocRoutineConfig(items=(
     m.LightSubConfig(what=[Light.LIVING_ROOM_DESK_LAMP, Light.LIVING_ROOM_FLOOR_LAMP], state="on"),
     m.LightSubConfig(what=Light.ENTANCE_DESK_LAMP, state=100),
     m.LightSubConfig(what=[Light.OFFICE_DESK_LAMP, Light.OFFICE_FLOOR_LAMP, Light.KITCHEN_LIGHTS], state="off"),
