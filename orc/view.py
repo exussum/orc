d131eeb9837265a2d36a5023785a41961b99a886
exussum <exussum@gmail.com>
2026-02-16 20:22:23 -0500

    Centralise css; move endpoints to /api; Add skip function

--- orc/view.py
+++ orc/view.py
@@ -39,20 +39,36 @@ class VersionManager:
 
 @bp.route("/")
 def index():
-    return (
-        render_template(
-            "button.html",
-            other_configs=config.OTHER_CONFIGS,
-            room_configs=config.ROOM_CONFIGS,
-            theme_configs=config.THEME_CONFIGS,
-            schedule_routines=config.SCHEDULE_ROUTINES,
-        ),
-        200,
-        {"Cache-control": "max-age=604800"},
+    eligible = {r.name for (_, r) in api.get_schedule(app.config_manager) if not any(cfg.mandatory for cfg in r.items)}
+    jobs = sorted(api.non_cron_jobs(app.scheduler), key=lambda e: e.trigger.run_date)
+    next_schedule = next((e for e in jobs if e.name in eligible and e.next_run_time), None)
+
+    return render_template(
+        "button.html",
+        other_configs=config.OTHER_CONFIGS,
+        room_configs=config.ROOM_CONFIGS,
+        theme_configs=config.THEME_CONFIGS,
+        schedule_routines=config.SCHEDULE_ROUTINES,
+        next_routine=next_schedule,
+        version=app.version_manager.version,
     )
 
 
-@bp.route("/remote/<id>")
+@bp.route("/schedule/")
+def schedule():
+    jobs = sorted(api.non_cron_jobs(app.scheduler), key=lambda e: e.trigger.run_date)
+    theme_override = app.config_manager.theme_override
+
+    theme = theme_override._replace(start=theme_override.start.isoformat(), end=theme_override.end.isoformat()) if theme_override else None
+
+    return (
+        render_template("schedule.html", version=app.version_manager.version, jobs=jobs, theme=theme),
+        200,
+        {"Cache-control": "no-store"},
+    )
+
+
+@bp.route("/api/remote/<id>")
 def remote(id):
     if id in ("TV Lights", "Partial TV Lights"):
         end = api.local_now() + timedelta(hours=3)
@@ -63,7 +79,7 @@ def remote(id):
     return {}, 200
 
 
-@bp.route("/console/<id>")
+@bp.route("/api/console/<id>")
 def console(id):
     if id == "Test":
         end = api.local_now() + timedelta(minutes=10)
@@ -83,10 +99,12 @@ def console(id):
         api.execute(config.SCHEDULE_ROUTINES[id])
     elif id in config.THEME_CONFIGS:
         api.execute(api.squish_configs(m.Configs(*config.ROUTINE_RESET_LIGHT.items), config.THEME_CONFIGS[id]))
+    else:
+        raise Exception("Unknown routine")
     return {}, 200
 
 
-@bp.route("/room/<id>")
+@bp.route("/api/room/<id>")
 def room(id):
     state = request.args.get("state")
     if state == config.ON:
@@ -101,21 +119,8 @@ def room(id):
     return {}, 200
 
 
-@bp.route("/schedule/")
-def schedule():
-    jobs = sorted(api.non_cron_jobs(app.scheduler), key=lambda e: e.trigger.run_date)
-    theme_override = app.config_manager.theme_override
-
-    theme = theme_override._replace(start=theme_override.start.isoformat(), end=theme_override.end.isoformat()) if theme_override else None
-
-    return (
-        render_template("schedule.html", version=app.version_manager.version, jobs=jobs, theme=theme),
-        200,
-        {"Cache-control": "no-store"},
-    )
-
-
-@bp.route("/schedule/set_theme", methods=["POST"])
+@bp.route("/api/schedule/set_theme", methods=["POST"])
+@VersionManager.versioned
 def set_theme():
     if not request.form["theme"]:
         app.config_manager.theme_override = None
@@ -127,10 +132,9 @@ def set_theme():
         )
     app.scheduler.remove_all_jobs()
     api.setup_scheduler(app.scheduler, app.config_manager)
-    return redirect("/schedule/")
 
 
-@bp.route("/schedule/<id>/pause")
+@bp.route("/api/schedule/<id>/pause")
 @VersionManager.versioned
 def pause(id):
     job = app.scheduler.get_job(id)
@@ -140,7 +144,7 @@ def pause(id):
         job.resume()
 
 
-@bp.route("/schedule/<id>/run")
+@bp.route("/api/schedule/<id>/run")
 @VersionManager.versioned
 def run(id):
     job = app.scheduler.get_job(id)
