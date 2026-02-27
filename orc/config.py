18ed55fbcbb58d603e328e085ba4e7075bac054d
exussum <exussum@gmail.com>
2026-02-27 06:57:54 -0500

    After config.md refactor, state numbers were created as strings - fix that; pass in config via env var

--- orc/config.py
+++ orc/config.py
@@ -12,6 +12,7 @@ SUNRISE_URL = os.getenv("SUNRISE_URL", "http://sunrise.example.com")
 MARKET_HOLIDAYS_URL = os.getenv("MARKET_HOLIDAYS_URL", "http://holidays.exmaple.com")
 SSL_KEY = os.getenv("SSL_KEY", "")
 SSL_CERT = os.getenv("SSL_CERT", "")
+ORC_CONFIG = os.getenv("ORC_CONFIG", "config.md")
 ENABLED = os.getenv("ENABLED", "")
 
 hubitat_config = dal.get_config() if ENABLED else {}
@@ -20,7 +21,7 @@ TZ = ZoneInfo("America/New_York")
 OFF = "off"
 ON = "on"
 
-with open("config.md") as fh:
+with open(ORC_CONFIG) as fh:
     doc = Document("".join(fh.readlines()))
 
 Light = m.build_enum(doc, "Devices", "Light", hubitat_config)
@@ -30,10 +31,10 @@ THEMES = m.build_themes(doc, "Routines", "Themes", Light, Sound)
 ROOM_CONFIGS = m.build_config(doc, "Room Configs", Light, Sound)
 THEME_CONFIGS = m.build_config(doc, "Ad-Hoc Routines", Light, Sound)
 OTHER_CONFIGS = m.build_expr_config(doc, "Super Routines", Light, Sound)
-
+ALL_CONFIGS = OTHER_CONFIGS | THEME_CONFIGS | ROOM_CONFIGS
 
 ROOM_CONFIGS_OFF = m.squish_configs(*ROOM_CONFIGS.values(), state_override=OFF)
-SCHEDULE_ROUTINES = {e.name: e for e in THEMES}
+SCHEDULE_ROUTINES = {r.name: r for e in THEMES for r in e.configs}
 
 DEFAULT_CONFIG = ROOM_CONFIGS["Living Room"]
-ALL_CONFIGS = OTHER_CONFIGS | THEME_CONFIGS | ROOM_CONFIGS
+RESET_CONFIG = m.squish_configs(m.Configs(*SCHEDULE_ROUTINES["Reset"].items))
