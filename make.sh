8c47d23f2b4b0bbdadf4ebfe25f9b4596c39aba3
exussum <exussum@gmail.com>
2026-05-25 18:52:43 -0400

    Fix tpyo

--- make.sh
+++ make.sh
@@ -3,10 +3,10 @@
 pip install twine==6.2.0 build==1.5.0
 rm -rf dist
 python3 -m build --wheel
-twine upload -u a -p a --repository-url http://registry.exussum.org:8080 dist/orc-*.whl
+twine upload -u a -p a --repository-url http://registry.int.exussum.org:8080 dist/orc-*.whl
 
 if [ "$1" = "full" ]; then
     rm -rf data/dist
     python3 -m build --wheel data
-    twine upload -u a -p a --repository-url http://registry.exussum.org:8080 data/dist/orc_data-*.whl
+    twine upload -u a -p a --repository-url http://registry.int.exussum.org:8080 data/dist/orc_data-*.whl
 fi
