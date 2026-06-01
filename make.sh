149c70f8f1ec432e23404a8c5ba7716c2e63931c
exussum <exussum@gmail.com>
2026-06-01 09:18:26 -0400

    Use pyproject.toml to install deps

--- make.sh
+++ make.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-pip install twine==6.2.0 build==1.4.4
+pip install -e '.[build]'
 rm -rf dist
 python3 -m build --wheel
-twine upload -u a -p a --repository-url http://registry.int.exussum.org:8080 dist/orc-*.whl
+python3 -m twine upload -u a -p a --repository-url http://registry.int.exussum.org:8080 dist/orc-*.whl
 
 if [ "$1" = "full" ]; then
     rm -rf data/dist
     python3 -m build --wheel data
-    twine upload -u a -p a --repository-url http://registry.int.exussum.org:8080 data/dist/orc_data-*.whl
+    python3 -m twine upload -u a -p a --repository-url http://registry.int.exussum.org:8080 data/dist/orc_data-*.whl
 fi
