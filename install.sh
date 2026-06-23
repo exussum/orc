eb5aac0cb16ce06b016bf46ec16caf223a16174b
exussum <exussum@gmail.com>
2026-06-23 19:23:34 -0400

    Shush uv warning

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 . .venv-orc/bin/activate
 supervisorctl stop orc
-/root/.local/bin/uv pip install --no-cache --reinstall --no-deps --index-url http://registry.example.local --trusted-host registry.example.local orc
+/root/.local/bin/uv pip install --no-cache --link-mode=copy --reinstall --no-deps --index-url http://registry.example.local --trusted-host registry.example.local orc
 supervisorctl start orc
 tail -f /var/log/orc.log
