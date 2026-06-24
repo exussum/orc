04f1e7ea485033875da746e20c02140bfc88bfc6
exussum <exussum@gmail.com>
2026-06-24 06:31:55 -0400

    Add deps install step with --reinstall-package orc and --index-strategy unsafe-best-match

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 . .venv-orc/bin/activate
 supervisorctl stop orc
+/root/.local/bin/uv pip install --no-cache --reinstall-package orc --index-url http://registry.int.exussum.org --extra-index-url https://pypi.org/simple/ --index-strategy unsafe-best-match --trusted-host registry.int.exussum.org orc
 /root/.local/bin/uv pip install --no-cache --link-mode=copy --reinstall --no-deps --index-url http://registry.int.exussum.org --trusted-host registry.int.exussum.org orc
 supervisorctl start orc
 tail -f /var/log/orc.log
