a9b38877a884e5c1e92cf068f0de53a77d61fc2d
exussum <exussum@gmail.com>
2026-06-25 10:14:21 -0400

    Consolidate orc install: single step, first-index, link-mode=copy

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 . .venv-orc/bin/activate
 supervisorctl stop orc
-/root/.local/bin/uv pip install --no-cache --reinstall-package orc --index-url http://registry.int.exussum.org --extra-index-url https://pypi.org/simple/ --index-strategy unsafe-best-match --trusted-host registry.int.exussum.org orc
-/root/.local/bin/uv pip install --no-cache --link-mode=copy --reinstall --no-deps --index-url http://registry.int.exussum.org --trusted-host registry.int.exussum.org orc
+/root/.local/bin/uv pip install --no-cache --link-mode=copy --reinstall-package orc --index-url http://registry.int.exussum.org --extra-index-url https://pypi.org/simple/ --index-strategy first-index --trusted-host registry.int.exussum.org orc==0.0.1
 supervisorctl start orc
 tail -f /var/log/orc.log
