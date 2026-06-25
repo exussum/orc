8959de41d8d63165385414157a2ab0851107cffa
exussum <exussum@gmail.com>
2026-06-25 19:56:40 -0400

    Fix the installs cript

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-. .venv-orc/bin/activate
+curl -sSL https://raw.githubusercontent.com/exussum/orc/main/pyproject.toml -o /tmp/pyproject.toml
+UV_PROJECT_ENVIRONMENT=/root/.venv-orc UV_LINK_MODE=copy UV_TRUSTED_HOST=registry.int.exussum.org /root/.local/bin/uv sync --no-install-project --no-cache --directory /tmp
 supervisorctl stop orc
-/root/.local/bin/uv pip install --no-cache --link-mode=copy --reinstall-package orc --index-url http://registry.int.exussum.org --extra-index-url https://pypi.org/simple/ --index-strategy first-index --trusted-host registry.int.exussum.org orc==0.0.1
+VIRTUAL_ENV=/root/.venv-orc /root/.local/bin/uv pip install orc==0.0.1 --no-deps --reinstall-package orc --link-mode=copy --index-url http://registry.int.exussum.org --trusted-host registry.int.exussum.org --no-cache
 supervisorctl start orc
 tail -f /var/log/orc.log
