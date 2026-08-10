62f9050b14569443800618070e661c6230ba7d25
exussum <exussum@gmail.com>
2026-08-10 17:00:19 -0400

    Prime the ssh master before scp since scp forces ControlMaster=no

--- scripts/build-and-install.sh
+++ scripts/build-and-install.sh
@@ -6,6 +6,7 @@ fi
 DEPLOY="${ORC_DEPLOY_USER:-root}@$ORC_DEPLOY_HOST"
 CTRL="-o ControlMaster=auto -o ControlPath=/tmp/orc-ssh-%r@%h:%p -o ControlPersist=60"
 sh scripts/upload.sh "$1" \
+    && ssh $CTRL "$DEPLOY" true \
     && scp $CTRL pyproject.toml "$DEPLOY:/tmp/pyproject.toml" \
     && ssh $CTRL "$DEPLOY" "ORC_REGISTRY_URL='$ORC_REGISTRY_URL' ORC_TRUSTED_HOST='$ORC_TRUSTED_HOST' bash -s" < scripts/install.sh
 
