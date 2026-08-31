4f08b929c72021f1087b28ed37e91219be2367c7
exussum <exussum@gmail.com>
2026-08-31 11:08:13 -0400

    Mock time.sleep globally in tests and consolidate same-object patches with patch.multiple

--- conftest.py
+++ conftest.py
@@ -1,3 +1,12 @@
 import os
+from unittest.mock import patch
+
+import pytest
 
 os.environ["ORC_CONFIG_DIR"] = "tests"
+
+
+@pytest.fixture(autouse=True)
+def no_sleep():
+    with patch("time.sleep"):
+        yield
